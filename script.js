a9915ed98fbc7cac516d63c4a08396e0e2e2e9f4
dericktante <156044704+dericktante@users.noreply.github.com>
2026-02-09 22:54:57 -0600

    Initial project detup with HTML, CSS, and Book constructor logic

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read){
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    return newBook;
+}
+
+addBookToLibrary("The Popper", "J.R.R. Tolkien", 310, true);
+console.log(myLibrary);
